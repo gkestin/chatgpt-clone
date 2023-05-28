a37f069f87e7c5f157b66ddd97d5068d682a6059
gkestin <107966538+gkestin@users.noreply.github.com>
2023-05-27 21:35:52 -0400

    ,

--- src/utils/chatGPT.js
+++ src/utils/chatGPT.js
@@ -24,7 +24,7 @@ export const chatGPT = async (prompt, chatbot_style, key) => {
 
   const response = await openai.createChatCompletion({
     //model: 'gpt-4.0',
-    model: 'gpt-3.5-turbo'
+    model: 'gpt-3.5-turbo',
     messages: messages,
   });
 
