c1fadd08597f67e2845b6a2cb308b78373501ce7
gkestin <107966538+gkestin@users.noreply.github.com>
2023-05-27 22:06:27 -0400

    new version based on working example put into chatGPT

--- src/utils/chatGPT.js
+++ src/utils/chatGPT.js
@@ -23,10 +23,9 @@ export const chatGPT = async (prompt, chatbot_style, key) => {
   ];
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-4.0',
-    //model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
     messages: messages,
   });
 
-  return response;
+  return response.data.choices[0].message.content;
 };
